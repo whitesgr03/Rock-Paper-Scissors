9c4bd60b8c784f75a384c053ccce57960d361f1f
Weiss Bai <whitesgr03@gmail.com>
2022-06-09 21:28:23 +0800

    Add script.js

---
+++ script.js
@@ -0,0 +1,86 @@
+// 開始猜拳
+function PlayRound(playSelection, computerSelection) {
+
+}
+
+// 玩家選擇
+function PlayerChoose() { 
+    let result = '';
+    do { 
+        result = prompt('請輸入 Rock, Paper, Scissors 開始猜拳');
+    }
+    while (result !== 'rock' || result !== 'paper' || result !== 'scissors' || result !== null)
+
+    if (result) {
+        return result.toLowerCase();
+    }
+
+    return false;
+}
+
+// 電腦選擇
+function ComputerChoose() { 
+
+}
+
+let PlayerSelection = PlayerChoose();
+
+// 玩家未選擇就終止
+if (PlayerSelection) {
+    let computerSelection = ComputerChoose();
+    PlayRound(PlayerSelection, computerSelection)
+}
+
+
+//  PROGRAM PlayRound
+//      選擇 = {1: 剪刀, 2: 石頭, 3: 布};
+//      玩家選擇 = "";
+//      電腦選擇 = "";
+//      PROGRAM PlayerSelection
+//          玩家選擇 = prompt().toLowerCase();
+//      END;
+//      PROGRAM ComputerPlay
+//          隨機數 = Math.random();
+//          IF (隨機數是 1);
+//              THEN 電腦選擇 = 剪刀;
+//              THEN    IF ( 隨機數是 2 );
+//                      THEN 電腦選擇 = 石頭;
+//                      ELSE 電腦選擇 = 布;
+//          ENDIF;
+//      END;
+//      IF (玩家選擇石頭 && 電腦選擇剪刀 ||
+//         玩家選擇剪刀 && 電腦選擇布 ||
+//         玩家選擇布 && 電腦選擇石頭);
+//             THEN 印出 You win! 玩家選擇 beats 電腦選擇;
+//             ELSE    IF (玩家選擇石頭 && 電腦選擇布 ||
+//                         玩家選擇剪刀 && 電腦選擇石頭 ||
+//                         玩家選擇布 && 電腦選擇剪刀);
+//                         THEN 印出 You lose! 電腦選擇 beats 玩家選擇;
+//                         ELSE 印出 Draw! 電腦選擇 玩家選擇;
+//                     ENDIF;
+//      ENDIF;
+//  END;
+
+
+
+//  PROGRAM RockPaperScissors
+//      比賽 = {win: 0, lose: 0};
+//      WHILE( 對戰未滿 5 場 )
+//          玩家選擇 = prompt();
+//          電腦選擇 = Math.random();
+//          PROGRAM ComputerPlay
+//              IF (玩家選擇石頭 && 電腦選擇剪刀 ||
+//                  玩家選擇剪刀 && 電腦選擇布 ||
+//                  玩家選擇布 && 電腦選擇石頭);
+//                      THEN 比賽的 win + 1;
+//                      ELSE    IF (玩家選擇石頭 && 電腦選擇布 ||
+//                                  玩家選擇剪刀 && 電腦選擇石頭 ||
+//                                  玩家選擇布 && 電腦選擇剪刀);
+//                                  THEN 比賽的 lose + 1;
+//                                  ELSE 印出平手;
+//                              ENDIF;
+//              ENDIF;
+//           END;
+//      ENDWHILE;
+//      印出比賽;
+//  END;
