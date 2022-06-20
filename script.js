5e61bd7d708c2b9e4647d6d53f246e65e30ca167
Weiss Bai <whitesgr03@gmail.com>
2022-06-20 08:57:21 +0800

    Add event listener run game, Change the way to play
    Update the score displayed and remove getPlayerChoose Fn

--- script.js
+++ script.js
@@ -23,25 +23,6 @@ function playRound(playSelection, computerSelection, round) {
     return result;
 }
 
-function getPlayerChoose() { 
-    let gesture = '';
-
-    // 未輸入手勢則持續循環
-    while (gesture !== 'rock' && gesture !== 'paper' && gesture !== 'scissors') {
-        
-        gesture = prompt('請輸入 Rock, Paper, Scissors 開始猜拳')
-
-        if (gesture === null) {
-            break;
-        } else {
-            // 將輸入的值保持一致小寫
-            gesture = gesture.toLowerCase()
-        }
-    }
-
-    return gesture;
-}
-
 function getComputerChoose() { 
     let gesture = { 1: 'rock', 2: 'paper', 3: 'scissors' };
 
@@ -54,50 +35,36 @@ function getComputerChoose() {
 
 function playGames() {
     let round = 1;
-    let score = { win: 0, lose: 0 }
+    let score = 0; 
 
-    while (round <= 5) {
-        let playerSelection = getPlayerChoose();
+    const win_score = document.querySelector('.win');
 
-        if (!playerSelection) {
-            alert('你已取消遊戲')
-            break;
-        }
+    const buttons = document.querySelectorAll('button');
 
+    buttons.forEach(button => button.addEventListener('click', function () {
+        let playerSelection = this.textContent.toLowerCase();
         let computerSelection = getComputerChoose();
         let result = playRound(playerSelection, computerSelection, round)
 
-        // 計分
         switch (result) {
             case 1: {
-                score.win++
-                break;
-            }
-            case -1: {
-                score.lose++
+                score++
                 break;
             }
         }
 
-        alert(`Round ${round}\nCurrent Score\nWin: ${score.win}, Lose: ${score.lose}`)
-        
+        win_score.textContent = score;
         round++
-    }
-
-    // 遊戲未滿 5 場提前終止
-    if (round !== 6) {
-        return
-    }
-
-    if (score.win > score.lose) {
-        alert(`You win the game!`)
-    } else if (score.win < score.lose) {
-        alert(`You lose the game!`)
-    } else {
-        alert(`The game was Drew!`)
-    }
+        
+        if (score === 5) {
+            setTimeout(() => {
+                alert(`You win the game!`)
+            }, 0);
+        }
+        
+    }));
+    
 }
 
-// 提示執行 5 輪遊戲
-alert('Start the 5 rounds game');
+alert(`Let's play Rock, Paper, Scissors!`)
 playGames();
